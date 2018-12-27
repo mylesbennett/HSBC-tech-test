a591c88a681a8a267a94437457afc2cc08e2b45d
Myles Bennett <myles.bennett@consultant.com>
2018-12-27 12:28:08 +0000

    used kotlin-android extensions and removed redundant findviewbyids

--- app/src/main/java/com/aimicor/hsbc/view/MainActivity.kt
+++ app/src/main/java/com/aimicor/hsbc/view/MainActivity.kt
@@ -3,20 +3,18 @@ package com.aimicor.hsbc.view
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.webkit.WebView
 import com.aimicor.hsbc.R
 import com.aimicor.hsbc.presenter.WebViewView
 import com.aimicor.hsbc.view.PresenterDelegate.Companion.presenterDelegate
 import com.github.satoshun.reactivex.webkit.data.OnPageFinished
 import com.github.satoshun.reactivex.webkit.events
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), WebViewView, PresenterAware {
     override val presenterDelegate = presenterDelegate<WebViewView>(this) {
         bindLifeCycle(lifecycle)
         bindView(this@MainActivity)
     }
-    private val webView by lazy { findViewById<WebView>(R.id.web_view) }
-    private val progressBar by lazy { findViewById<View>(R.id.progressbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
